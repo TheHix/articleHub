408967d8ea5b54cb22893c5f3e66b068c0476469
Sava <86553293+TheHix@users.noreply.github.com>
2023-03-03 11:35:31 +0300

    added global styles; css variables

---
+++ src/app/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "./styles/index.scss";
+
+import { useTheme } from "./providers/ThemeProvider";
+import { classNames } from "@/shared/lib/ClassNames/ClassNames";
+
+import { AppRouter } from "./providers/router";
+import { Navbar } from "@/widgets/Navbar";
+import { Sidebar } from "@/widgets/Sidebar";
+
+const App = () => {
+	const { theme } = useTheme();
+
+	return (
+		<div className={classNames("app", [theme], {})}>
+			<Navbar />
+			<div className="content">
+				<Sidebar />
+				<AppRouter />
+			</div>
+		</div>
+	);
+};
+
+export default App;
