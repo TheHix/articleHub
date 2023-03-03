745e3d76f0ace419ecb0e673375da143c5e2da70
Sava <86553293+TheHix@users.noreply.github.com>
2023-03-03 11:37:03 +0300

    updated tsconfig

---
+++ src/app/types/global.d.ts
@@ -0,0 +1,15 @@
+declare module "*.module.css";
+declare module "*.module.scss";
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg" {
+	import * as React from "react";
+
+	const ReactComponent: React.FunctionComponent<
+		React.SVGProps<SVGSVGElement> & { title?: string }
+	>;
+
+	export default ReactComponent;
+}
